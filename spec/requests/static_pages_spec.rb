e762b56eac4b6fc4d4a471c117d1beb0b9b5ee9e
thirotan <dev@vagrant-centos65.vagrantup.com>
2015-09-08 00:39:13 +0900

    specを少しスッキリさせた

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -1,65 +1,54 @@
 require 'rails_helper'
 
 RSpec.describe "StaticPages", type: :request do
+  let(:base_title) {"Ruby on Rails Tutorial Sample App"}
   describe "GET /static_pages/home" do
-
+    before { visit '/static_pages/home' }
     it "should have the content 'Sample App'" do
-      visit '/static_pages/home'
       expect(page).to have_content('Sample App')
     end
-
     it "should have the right title" do
-      visit '/static_pages/home'
-      expect(page).to have_title("Ruby on Rails Tutorial Sample App | Home")
+      expect(page).to have_title("#{base_title} | Home")
     end
-
-    it "should have response status equal 200 " do
-      get '/static_pages/home'
-      expect(response).to have_http_status(200)
-    end
+#     it "should have response status equal 200 " do
+#       get '/static_pages/home'
+#       expect(response).to have_http_status(200)
+#     end
   end
 
   describe "GET /static_pages/help" do
-
+    before { visit '/static_pages/help '}
     it "should have the content 'Help'" do 
-      visit '/static_pages/help'
       expect(page).to have_content('Help')
     end
-
     it "should have the right title" do
-      visit '/static_pages/help'
-      expect(page).to have_title("Ruby on Rails Tutorial Sample App | Help")
+      expect(page).to have_title("#{base_title} | Help")
     end
-
-    it "should have response status equal 200 " do
-      get  '/static_pages/help'
-      expect(response).to have_http_status(200)
-    end
+#    it "should have response status equal 200 " do
+#      get  '/static_pages/help'
+#      expect(response).to have_http_status(200)
+#    end
   end
 
   describe "GET /static_pages/about" do
-
+    before{ visit '/static_pages/about' }
     it "should have the content 'About Us'" do
-      visit '/static_pages/about'
       expect(page).to have_content('About Us')
     end
 
     it "should have the right title" do
-      visit '/static_pages/about'
-      expect(page).to have_title("Ruby on Rails Tutorial Sample App | About Us")
+      expect(page).to have_title("#{base_title} | About Us")
     end
   end
 
   describe "GET /static_pages/contact" do
-
+    before { visit '/static_pages/contact' }
     it "should hve the content 'Content'" do
-      visit '/static_pages/contact'
       expect(page).to have_content('Contact')
     end
     
     it "should have the right title" do
-      visit '/static_pages/contact'
-      expect(page).to have_title("Ruby on Rails Tutorial Sample App | Contact")
+      expect(page).to have_title("#{base_title} | Contact")
     end
 
   end
