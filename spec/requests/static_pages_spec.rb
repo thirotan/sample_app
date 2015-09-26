838aa319f487bae8adc5ab981479e4989932f616
thirotan <dev@vagrant-centos65.vagrantup.com>
2015-09-26 11:44:16 +0900

    add test share_example

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -3,20 +3,31 @@ require 'rails_helper'
 RSpec.describe "StaticPages", type: :request do
   let(:base_title) {"Ruby on Rails Tutorial Sample App"}
   subject { page }
+  shared_examples_for "all static pages" do
+    it { expect(page).to have_content(heading) }
+    it { expect(page).to have_title(page_title) }
+  end
   describe "GET /static_pages/home" do
     before {visit root_path}
-    it { expect(page).to have_content('Sample App') }
-    it { expect(page).to have_title(full_title('Home')) }
-#     it "should have response status equal 200 " do
-#       get '/static_pages/home'
-#       expect(response).to have_http_status(200)
-#     end
+
+    let(:heading) { 'Sample App' }
+    let(:page_title) { "#{base_title} | Home" }
+    it_behaves_like "all static pages"
+
+    it { expect(page).not_to have_title('home') }
+#    it "should have response status equal 200 " do
+#      get '/static_pages/home'
+#      expect(response).to have_http_status(200)
+#    end
   end
 
   describe "GET /static_pages/help" do
     before { visit help_path }
-    it { expect(page).to have_content('Help') }
-    it { expect(page).to have_title("#{base_title} | Help") }
+  
+    let(:heading) { 'Help' }
+    let(:page_title) { "#{base_title} | Help" }
+
+    it_behaves_like "all static pages"
 #    it "should have response status equal 200 " do
 #      get  '/static_pages/help'
 #      expect(response).to have_http_status(200)
@@ -25,13 +36,19 @@ RSpec.describe "StaticPages", type: :request do
 
   describe "GET /static_pages/about" do
     before{ visit about_path }
-    it { expect(page).to have_content('About Us') }
-    it { expect(page).to have_title("#{base_title} | About Us") }
+
+    let(:heading) { 'Help' }
+    let(:page_title) { "#{base_title} | About Us"}
+
+    it_behaves_like "all static pages"
   end
 
   describe "GET /static_pages/contact" do
     before { visit contact_path }
-    it { expect(page).to have_content('Contact') }
-    it { expect(page).to have_title("#{base_title} | Contact") }
+
+    let(:heading) { 'Contact' }
+    let(:page_title) { "#{base_title} | Contact" }
+
+    it_behaves_like "all static pages"
   end
 end
