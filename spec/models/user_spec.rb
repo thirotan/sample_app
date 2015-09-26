5a82a607c74eaef3e4e1c0e348a4b9166d62311a
thirotan <dev@vagrant-centos65.vagrantup.com>
2015-09-26 20:24:16 +0900

    fix enshu-6.5

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -31,7 +31,7 @@ RSpec.describe User, type: :model do
 
   describe "when email format is invalid" do
     it "should be invalid" do
-      addresses = %w[user@foo,com user_at_foo.org example.user@foo. foo@bar_bar.com foo@bar+baz.com]
+      addresses = %w[user@foo..com user_at_foo.org example.user@foo. foo@bar_bar.com foo@bar+baz.com]
       addresses.each do |invalid_address|
         @user.email = invalid_address
         is_expected.not_to be_valid
@@ -57,6 +57,16 @@ RSpec.describe User, type: :model do
     end
 
     it { is_expected.not_to be_valid }
+  end
+
+  describe "email address with mixed case" do
+    let(:mixed_case_email) { "Foo@ExAMPLe.Com" }
+ 
+    it "should be saved as all lower-case" do
+      @user.email = mixed_case_email
+      @user.save
+      expect(@user.reload.email).to eq mixed_case_email.downcase
+    end
   end
 
   describe "when password is not present" do
