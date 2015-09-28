a9ce536d0800e5d355d27c4708dd5ade0a8948f6
thirotan <dev@vagrant-centos65.vagrantup.com>
2015-09-28 20:36:52 +0900

    add enshu-7

--- spec/requests/user_pages_spec.rb
+++ spec/requests/user_pages_spec.rb
@@ -20,6 +20,13 @@ RSpec.describe "UserPages", type: :request do
       it "should not create a user" do
         expect{ click_button submit }.not_to change(User, :count)
       end
+     
+      describe "after submission" do
+        before { click_button submit }
+
+        it { is_expected.to have_title('Sign up') }
+        it { is_expected.to have_content('error') }
+      end
     end
 
     describe "with valid information" do
@@ -32,6 +39,14 @@ RSpec.describe "UserPages", type: :request do
 
       it "should create a user" do
         expect{ click_button submit }.to change(User, :count).by(1)
+      end
+
+      describe "after saving the user" do
+        before { click_button submit }
+        let(:user) { User.find_by(email: 'user@example.com') }
+
+        it { is_expected.to have_title(user.name) }
+        it { is_expected.to have_selector('div.alert.alert-success', text: 'Welcome') }
       end
     end
   end
